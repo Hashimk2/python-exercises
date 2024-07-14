37d0dfe478a29436cb90abf7f51f022a583c5acc
Hashim Kidwai <hashimkidwai@gmail.com>
2024-07-14 16:23:01 -0500

    Add find next day exercise

---
+++ conditionals/date.py
@@ -0,0 +1,112 @@
+# # # year_input = int(input('Please enter the current year: '))
+# # # month_input = int(input('Please enter the current month: '))
+# # # year_input = int(input('Please enter the current day: '))
+
+# January = 1
+# Febraury = 2
+# March = 3
+# April = 4
+# May = 5
+# June = 6
+# July = 7
+# August = 8
+# September = 9
+# October = 10
+# November = 11
+# December = 12
+
+
+
+# # # if int(month_input == 1):
+# # #   month_input = January
+# # # if int(month_input == 2):
+# # #  month_input = Febraury
+# # # if int(month_input == 3):
+# # #  month_input = March
+# # # if int(month_input == 4):
+# # #  month_input = April
+# # # if int(month_input == 5):
+# # #  month_input = May
+# # # if int(month_input == 6):
+# # #  month_input = June
+# # # if int(month_input == 7):
+# # #  month_input = July
+# # # if int(month_input == 8):
+# # #  month_input = August
+# # # if int(month_input == 9):
+# # #  month_input = September
+# # # if int(month_input == 10):
+# # #  month_input = October
+# # # if int(month_input == 11):
+# # #  month_input = November
+# # # if int(month_input == 12):
+# # #  month_input = December
+
+# # # date = (year_input + month_input + year_input)
+# # # print(date)
+
+
+# # date_input = int(input('Please enter the current year, month, and day: '))
+# # print(date_input)
+
+# user_input = int(input('Please enter the current year: '))
+# print(user_input)
+# user_input_2 = int(input('Please enter the current month as a number (ie. March = 3): '))
+# print(user_input_2)
+# user_input_3 = int(input('Please enter the current day: '))
+# print(user_input_3)
+# if str(user_input_2 == 1):
+#   print('January')
+# if str(user_input_2 == 2):
+#  print('Febraury')
+# if str(user_input_2 == 3):
+#  print('March')
+# if str(user_input_2 == 4):
+#  print('April')
+# if str(user_input_2 == 5):
+#   print('May')
+# if str(user_input_2 == 6):
+#  print('June')
+# if str(user_input_2 == 7):
+#  print('July')
+# if str(user_input_2 == 8):
+#   print('August')
+# if str(user_input_2 == 9):
+#   print('September')
+# if str(user_input_2 == 10):
+#   print('October')
+# if str(user_input_2 == 11):
+#   print('November')
+# if str(user_input_2 == 12):
+#   print('December')
+# print(user_input + user_input_2 + user_input_3)
+
+year_input = int(input('Please enter the current year:  '))
+month_input = int(input('Please enter the current month as a number (ie. March = 3):  '))
+day_input = (input('Please enter the current day:  '))
+
+if (month_input == 1):
+  month_input = 'January'
+if (month_input == 2):
+  month_input = 'January'
+if (month_input == 3):
+  month_input = 'January'
+if (month_input == 4):
+  month_input = 'January'
+if (month_input == 5):
+  month_input = 'January'
+if (month_input == 6):
+  month_input = 'June'
+if (month_input == 7):
+  month_input = 'July'
+if (month_input == 8):
+  month_input = 'August'
+if (month_input == 9):
+  month_input = 'September'
+if (month_input == 10):
+  month_input = 'October'
+if (month_input == 11):
+  month_input = 'November'
+if (month_input == 12):
+  month_input = 'December'
+
